4fc8e31eb94c1fcbb17b38b18a29b96efc90b1d3
Denis Kabana <8308865+deniskabana@users.noreply.github.com>
2023-01-08 06:05:38 +0100

    NextJS docs publishing pipeline

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: "",
+  basePath: "",
+};
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  nextConfig.assetPrefix = `/${repo}/`;
+  nextConfig.basePath = `/${repo}`;
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
